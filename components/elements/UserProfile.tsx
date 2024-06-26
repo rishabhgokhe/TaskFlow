f619f0c3e1f89aa432c866586473d3e996e03f56
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2024-06-26 15:35:36 +0530

    fixed deployment issue

--- components/elements/UserProfile.tsx
+++ components/elements/UserProfile.tsx
@@ -1,5 +1,5 @@
-import { auth } from '@/auth';
-import { useSession, SessionProvider } from 'next-auth/react';
+// import { auth } from '@/auth';
+// import { useSession, SessionProvider } from 'next-auth/react';
 import Image from 'next/image';
 import React from 'react';
 
