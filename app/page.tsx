4c5506ca7b3ad1006ab215e9afd6dd47deacb930
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2024-06-30 18:43:39 +0530

    added sparkle text in landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen p-4">
       <div className="flex w-full justify-between items-center">
-        <h1 className="font-bold text-3xl mb-4 text-center flex-1">Welcome to TaskFlow</h1>
+        <h1 className="font-bold text-xl sm:text-3xl mb-4 text-center flex-1">Welcome to TaskFlow</h1>
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ml-auto">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="relative inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-4 py-2 text-sm font-medium text-white backdrop-blur-3xl tracking-wide">
